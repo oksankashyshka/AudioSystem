b1d0f767f8e55ed77b3f5451318f2495c527c8f5
Honesty <oksanka.shyshka@gmail.com>
2016-02-02 20:10:14 +0200

    Add class Song

---
+++ SmallProjAudiosystem/Song.h
@@ -0,0 +1,36 @@
+#pragma once
+class CSong
+{
+public:
+	CSong();
+	~CSong();
+};
+
+/*
+class Song
+{
+public:
+Song()//default construct
+{
+m_sTitle = "";
+m_sLyrics = "";
+m_nSeconds = 0;
+}
+Song( string sTitle, int nSeconds, string sLyrics)//construct with parameters
+{
+m_sTitle = sTitle;
+m_sLyrics = sLyrics;
+m_nSeconds = nSeconds;
+}
+
+string GetTitle() { return m_sTitle; }	//function that give(return) to us title
+string GetLyrics() { return m_sLyrics; }//function that give(return) to us lyrics
+int GetDuration() { return m_nSeconds; }// -//- to us duration in sec
+//var
+private:
+string m_sTitle;//singer or group + name of song
+int m_nSeconds;//duration of song in seconds
+string m_sLyrics;//small part of lyrics
+};
+
+*/
